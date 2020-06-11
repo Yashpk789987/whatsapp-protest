6cd254679f46ede46b35dba42ba6ceef393f923a
Yashpk789987 <yashpk789987@gmail.com>
2020-06-12 02:44:45 +0530

    Initial Release

---
+++ messageLoop/utils.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+
+
+
+
+
+# driver.quit()
